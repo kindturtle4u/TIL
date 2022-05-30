d76a16b13c01b6ae75510f87e5c36db2eedf2787
namsangjae <kindturtle4u@gmail.com>
2022-05-30 17:16:01 +0900

    TIL

---
+++ Spring/[책]스프링부트 쇼핑몰 프로젝트 with JPA/project/shop/src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.shop.repository;
+
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+
+        Item saveItem = itemRepository.save(item);
+        System.out.println(saveItem.toString());
+    }
+}
