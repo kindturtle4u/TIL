36023cd5f22ec4a6f1edb2c7d9287713e9413a00
namsangjae <kindturtle4u@gmail.com>
2022-06-13 16:35:14 +0900

    TIL

---
+++ Spring/[책]스프링부트 쇼핑몰 프로젝트 with JPA/project/shop/src/main/java/com/shop/service/MemberService.java
@@ -0,0 +1,26 @@
+package com.shop.service;
+
+import com.shop.entity.Member;
+import com.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member) {
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if (findMember != null ) {
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+}
