d5fbc1d7732bd5b10a6ed18bd66c7ae2ad11f7bb
namsangjae <kindturtle4u@gmail.com>
2022-07-06 10:55:00 +0900

    TIL

--- Spring/[책]스프링부트 쇼핑몰 프로젝트 with JPA/project/shop/src/main/java/com/shop/controller/ItemController.java
+++ Spring/[책]스프링부트 쇼핑몰 프로젝트 with JPA/project/shop/src/main/java/com/shop/controller/ItemController.java
@@ -1,13 +1,16 @@
 package com.shop.controller;
 
+import com.shop.dto.ItemFormDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ItemController {
 
     @GetMapping("/admin/item/new")
-    public String itemForm() {
+    public String itemForm(Model model) {
+        model.addAttribute("itemFormDto", new ItemFormDto());
         return "/item/itemForm";
     }
 }
