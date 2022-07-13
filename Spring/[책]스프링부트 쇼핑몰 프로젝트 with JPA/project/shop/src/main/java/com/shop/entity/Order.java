784b3eb42ae0a8126494d3b81ab9ef6379b8c9aa
namsangjae <kindturtle4u@gmail.com>
2022-07-13 10:51:47 +0900

    TIL

--- Spring/[책]스프링부트 쇼핑몰 프로젝트 with JPA/project/shop/src/main/java/com/shop/entity/Order.java
+++ Spring/[책]스프링부트 쇼핑몰 프로젝트 with JPA/project/shop/src/main/java/com/shop/entity/Order.java
@@ -34,4 +34,29 @@ public class Order extends BaseEntity{
 
     private LocalDateTime updateTime;
 
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public static Order createOrder(Member member, List<OrderItem> orderItemList) {
+        Order order = new Order();
+        order.setMember(member);
+        for (OrderItem orderItem : orderItemList) {
+            order.addOrderItem(orderItem);
+        }
+        order.setOrderStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
+    }
+
 }
