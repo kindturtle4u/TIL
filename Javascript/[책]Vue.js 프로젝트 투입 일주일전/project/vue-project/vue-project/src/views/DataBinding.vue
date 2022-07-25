3ecabf72f014589bb1ff67e461a1d1687454b687
namsangjae <kindturtle4u@gmail.com>
2022-07-25 11:33:07 +0900

    TIL

---
+++ Javascript/[책]Vue.js 프로젝트 투입 일주일전/project/vue-project/vue-project/src/views/DataBinding.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>hello, {{ title }}!</h1>
+
+    <div>{{htmlString}}</div>
+    <div v-html="htmlString"></div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'World',
+            htmlString: '<p style="color:red;">This is a red string.</p>'
+        };
+    },
+    name: "DataBinding"
+}
+</script>
+
+<style scoped>
+
+</style>
