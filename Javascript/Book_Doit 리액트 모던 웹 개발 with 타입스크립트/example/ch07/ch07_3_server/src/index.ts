1ca6420770db84a935d63dd5230c85c7ee977e72
namsangjae <kindturtle4u@gmail.com>
2023-01-12 13:41:25 +0900

    TIL

---
+++ Javascript/Book_Doit 리액트 모던 웹 개발 with 타입스크립트/example/ch07/ch07_3_server/src/index.ts
@@ -0,0 +1,18 @@
+import {createServer} from 'http'
+import {getPublicDirPath} from './config'
+import {makeDir} from './utils'
+import {createExpressApp} from './express'
+import type {MongoDB} from './mongodb'
+import {connectAndUseDB} from './mongodb'
+
+makeDir(getPublicDirPath())
+
+const connectCallback = (db: MongoDB) => {
+  const hostname = 'localhost',
+    port = 4000
+
+  createServer(createExpressApp(db)).listen(port, () =>
+    console.log(`connect http://${hostname}:${port}`)
+  )
+}
+connectAndUseDB(connectCallback, 'ch07')
