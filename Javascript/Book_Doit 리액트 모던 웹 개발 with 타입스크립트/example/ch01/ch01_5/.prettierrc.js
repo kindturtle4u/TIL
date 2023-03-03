40ef5247d34ac1d1bc3fde7856a5e8bf86d4ac1e
namsangjae <kindturtle4u@gmail.com>
2023-03-03 17:52:59 +0900

    TIL

--- Javascript/Book_Doit 리액트 모던 웹 개발 with 타입스크립트/example/ch01/ch01_5/.prettierrc.js
+++ Javascript/Book_Doit 리액트 모던 웹 개발 with 타입스크립트/example/ch01/ch01_5/.prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
-  singleQuote: true,
-  trailingComma: 'none',
-  arrowParens: 'avoid',
-semi: false,
-printWidth: 90
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    trailingComma: 'none',
+    arrowParens: 'avoid',
+    semi: false,
+    printWidth: 90
 };
