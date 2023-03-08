7320e734b82f66fac4c90c395716695f665e0d69
namsangjae <kindturtle4u@gmail.com>
2023-03-08 11:42:09 +0900

    TIL

---
+++ Spring/인프런_스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/practice/hello-spring/src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
