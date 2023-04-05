9cfd7a206ffe056c333216470c32191451e42821
namsangjae <kindturtle4u@gmail.com>
2023-04-05 16:36:32 +0900

    TIL

--- Spring/인프런_스프링핵심원리_기본편/practice/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ Spring/인프런_스프링핵심원리_기본편/practice/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,20 +1,16 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
