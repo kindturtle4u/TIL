96c71a245ada0807016d09f7638b05f0283a74ca
namsangjae <kindturtle4u@gmail.com>
2024-01-13 19:33:52 +0900

    TIL

--- Spring/인프런_로드맵 스프링부트와 JPA 실무 완전정복/인프런_로드맵2_실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/practice/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ Spring/인프런_로드맵 스프링부트와 JPA 실무 완전정복/인프런_로드맵2_실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발/practice/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,21 +1,31 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import jakarta.persistence.criteria.*;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
-import lombok.RequiredArgsConstructor;
+import jpabook.jpashop.domain.OrderStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static jpabook.jpashop.domain.QMember.member;
+import static jpabook.jpashop.domain.QOrder.order;
+
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
     private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     public void save(Order order) {
         em.persist(order);
@@ -25,7 +35,7 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch) {
+    public List<Order> findAll2(OrderSearch orderSearch) {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Order> cq = cb.createQuery(Order.class);
         Root<Order> o = cq.from(Order.class);
@@ -53,6 +63,36 @@ public class OrderRepository {
 
         return query.getResultList();
     }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return query.select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch))
+                .limit(1000)
+                .fetch();
+    }
+
+    private static BooleanExpression nameLike(OrderSearch orderSearch) {
+        if (!StringUtils.hasText(orderSearch.getMemberName())) {
+            return null;
+        }
+
+        return member.name.like(orderSearch.getMemberName());
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) return null;
+
+        return order.status.eq(statusCond);
+    }
+
+    private BooleanExpression statusEq2(OrderStatus statusCond) {
+        if (statusCond == null) return null;
+
+        return order.status.eq(statusCond);
+    }
+
 
     public List<Order> findAllWithMemberDelivery(OrderSearch orderSearch) {
         return em.createQuery("""
