a9b8c008d2854df6c173880d467ed3b44a235425
namsangjae <kindturtle4u@gmail.com>
2024-01-15 00:38:02 +0900

    TIL

--- Spring/인프런_로드맵 스프링부트와 JPA 실무 완전정복/인프런_로드맵4_실전! 스프링 데이터 JPA/practice/data-jpa/src/main/java/study/datajpa/entity/Member.java
+++ Spring/인프런_로드맵 스프링부트와 JPA 실무 완전정복/인프런_로드맵4_실전! 스프링 데이터 JPA/practice/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -1,23 +1,39 @@
 package study.datajpa.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Entity
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@ToString(exclude = "team")
 public class Member {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
 
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        changeTeam(team);
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
