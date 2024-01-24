ffa213647f0e160a927e78800b82405ee60cb02a
namsangjae <kindturtle4u@gmail.com>
2024-01-24 14:44:53 +0900

    TIL

--- Javascript/책_Doit 리액트 모던 웹 개발 with 타입스크립트/practice/ch02/ch02_5/src/pages/OnChange.tsx
+++ Javascript/책_Doit 리액트 모던 웹 개발 with 타입스크립트/practice/ch02/ch02_5/src/pages/OnChange.tsx
@@ -1,3 +1,28 @@
-export default function CopyMe() {
-    return <div>CopyMe</div>
+import {ChangeEvent} from "react";
+
+export default function OnChange() {
+    const onChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+        e.stopPropagation()
+        e.preventDefault()
+        console.log('onChangeValue', e.target.value);
+    }
+
+    const onChangeChecked = (e: ChangeEvent<HTMLInputElement>) => {
+        e.stopPropagation()
+        console.log('onChangeChecked', e.target.checked)
+    }
+
+    const onChangeFiles = (e: ChangeEvent<HTMLInputElement>) => {
+        e.stopPropagation()
+        console.log('onChangeFiles', e.target.files)
+    }
+
+    return (
+        <div>
+            <p>OnChange</p>
+            <input type="text" onChange={onChangeValue} placeholder="type some text" defaultValue="hello"/>
+            <input type="checkbox" onChange={onChangeChecked} defaultChecked/>
+            <input type="file" onChange={onChangeFiles} multiple accept="image/*"/>
+        </div>
+    )
 }
