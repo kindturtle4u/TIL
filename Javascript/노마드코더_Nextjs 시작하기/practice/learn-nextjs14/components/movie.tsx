62280dff8e8ea391fa8fab5dfa4440332e649819
namsangjae <kindturtle4u@gmail.com>
2024-04-04 17:38:44 +0900

    TIL

--- Javascript/노마드코더_Nextjs 시작하기/practice/learn-nextjs14/components/movie.tsx
+++ Javascript/노마드코더_Nextjs 시작하기/practice/learn-nextjs14/components/movie.tsx
@@ -16,7 +16,7 @@ export default function Movie({id, poster_path, title}: IMovieProps) {
     return (
         <div className={styles.movie}>
             <img src={poster_path} alt={title} onClick={onClick}/>
-            <Link href={`/movies/${id}`}>{title}</Link>
+            <Link prefetch href={`/movies/${id}`}>{title}</Link>
         </div>
     );
 }
