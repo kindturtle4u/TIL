f5ffd26b4b0062d2e5c59881e0c85ff21aec35e3
남상재 <kindturtle4u@gmail.com>
2024-10-17 14:12:25 +0900

    TIL

--- Java/인프런_김영환 실전 자바_고급1편 멀티스레드와 동시성/practice/java-adv1/src/thread/control/sync/lock/LockSupportMainV1.java
+++ Java/인프런_김영환 실전 자바_고급1편 멀티스레드와 동시성/practice/java-adv1/src/thread/control/sync/lock/LockSupportMainV1.java
@@ -1,8 +1,24 @@
 package thread.control.sync.lock;
 
+import java.util.concurrent.locks.LockSupport;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
 public class LockSupportMainV1 {
 
     public static void main(String[] args) {
+        Thread thread1 = new Thread(new ParkTest(), "Thread-1");
+        thread1.start();
+
+        // 잠시 대기하여 Thread-1이 park 상태에 빠질 시간을 준다.
+        sleep(100);
+
+        log("Thead-1 state: " + thread1.getState());
+
+        log("main -> unpark(Thread-1)");
+        LockSupport.unpark(thread1); // 1. unpark 사용
+        //thread1.interrupt(); // 2. interrupt 사용
 
     }
 
@@ -10,7 +26,10 @@ public class LockSupportMainV1 {
 
         @Override
         public void run() {
-            log("ㅔ")
+            log("park 시작");
+            LockSupport.park();
+            log("park 종료, state: " + Thread.currentThread().getState());
+            log("인터럽트 상태: " + Thread.currentThread().isInterrupted());
         }
     }
 }
